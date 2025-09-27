b6f86cf36b8e6265bb1a76ea894df4659f0a57d2
Yuga-Nagata <yuga.nagata.com@gmail.com>
2025-09-27 19:49:51 +0900

    💄 SP時のスライダーのデザインを修正

--- marinaka/main.js
+++ marinaka/main.js
@@ -1,18 +1,28 @@
-'use strict';
+"use strict";
 
 {
-    $('.img__05').slick({
-        infinite:true,
-        speed:1000,
-        slidesToShow:3,
-        autoplay:true,
-        autoplaySpeed:2000,
-    });
+  $(".img__05").slick({
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 
-    $('.top_btn').on('click',function(){
-        $('html,body').animate({
-            scrollTop: 0
-        },600);
-    })
-
+  $(".top_btn").on("click", function () {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      600
+    );
+  });
 }
